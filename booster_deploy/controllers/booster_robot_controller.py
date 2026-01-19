f8cc6a35d5dea70f2f66d00dfe319016f5cfe0af
SavvyProp <ludwigtaycheeying@gmail.com>
2026-01-19 15:43:04 -0500

    no vel

--- booster_deploy/controllers/booster_robot_controller.py
+++ booster_deploy/controllers/booster_robot_controller.py
@@ -527,57 +527,8 @@ class BoosterRobotController(BaseController):
         self.robot.data.root_ang_vel_b = torch.from_numpy(
             state["root_ang_vel_b"]).to(dtype=torch.float32).to(
                 self.robot.data.device)
-        try:
-            self.portal.logger.info("Getting marker position from Vicon...")
-            vicon_pos, vicon_quat, rpy = self.vicon_client.get_marker_position(
-                "Booster/booster_seg"
-                )
-
-            marker_offset_body = np.array([0.150, 0.0, 0.162]) # top-center of Booster
-            R_meas_body = np.array([[1.0, 0.0, 0.0],
-                                        [0.0, 0.0, 1.0],
-                                        [0.0,-1.0, 0.0]])
-
-            # Additional fixed pitch tilt of the marker plane by +6 deg about BODY Y (tilt defined in true body frame)
-            theta = np.deg2rad(18.36)
-            R_body_markers = np.array([[ np.cos(theta), 0.0, np.sin(theta)],
-                                        [ 0.0,          1.0, 0.0         ],
-                                        [-np.sin(theta), 0.0, np.cos(theta)]])
-
-            # Total body->measured mapping including mounting tilt (apply body tilt first, then body->measured axis mapping)
-            marker_offset_meas = R_meas_body @ (R_body_markers @ marker_offset_body)
-            cr, sr = np.cos(rpy[0]), np.sin(rpy[0])
-            cp, sp = np.cos(rpy[1]), np.sin(rpy[1])
-            cy, sy = np.cos(rpy[2]), np.sin(rpy[2])
-            R_x = np.array([[1.0, 0.0, 0.0],
-                                [0.0,  cr, -sr],
-                                [0.0,  sr,  cr]])
-            R_y = np.array([[ cp, 0.0, sp],
-                                [0.0, 1.0, 0.0],
-                                [-sp, 0.0, cp]])
-            R_z = np.array([[ cy, -sy, 0.0],
-                                [ sy,  cy, 0.0],
-                                [0.0, 0.0, 1.0]])
-            R_world_meas = R_z @ R_y @ R_x
-            R_world_body = R_world_meas @ (R_meas_body @ R_body_markers)
-
-            st = time.time()
-            dt = st - self.last_time
-            self.last_time = st
-            alpha = 0.01
-            raw_global_vel = (vicon_pos - self.vicon_pos) / dt
-            self.vicon_pos = vicon_pos
-            self.global_vel = self.global_vel * (1 - alpha) + raw_global_vel * alpha
-
-            self.local_vel = np.linalg.inv(R_world_body) @ self.global_vel
-            
-            self.robot.data.root_lin_vel_b = torch.from_numpy(
-                self.local_vel).to(dtype=torch.float32).to(self.robot.data.device)
-            self.portal.logger.info("Local vel x: {:.3f} y: {:.3f} z: {:.3f}".format(self.local_vel[0], self.local_vel[1], self.local_vel[2]))
-        except Exception as e:
-            print("Failed to get marker position:", e)
-            self.robot.data.root_lin_vel_b = torch.from_numpy(
-                self.local_vel).to(
+        self.robot.data.root_lin_vel_b = torch.from_numpy(
+            self.local_vel).to(
                     dtype=torch.float32).to(self.robot.data.device)
 
     def ctrl_step(self, dof_targets: torch.Tensor) -> None:
