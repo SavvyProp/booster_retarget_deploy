8415b3f871b73409eea1cbc8c42b875e87c4a878
SavvyProp <ludwigtaycheeying@gmail.com>
2026-02-04 16:55:20 -0500

    old grav vec

--- tasks/lcc_retarget/lcc_retarget.py
+++ tasks/lcc_retarget/lcc_retarget.py
@@ -190,9 +190,9 @@ class LCCRetargetPolicy(Policy):
             self.decimation_counter = 0
 
         r_wb = quat_to_rotmat_wxyz(base_quat)
-        if self.robot.data.grav_vec_b[2] == 0.0:
-            grav_vec = r_wb.T @ np.array([0.0, 0.0, -9.81], dtype=np.float32)
-        else:
+        grav_vec = r_wb.T @ np.array([0.0, 0.0, -9.81], dtype=np.float32)
+        print("old Gravity vector: ", grav_vec)
+        if self.robot.data.grav_vec_b[2] != 0.0:
             grav_vec = self.robot.data.grav_vec_b
         print("Gravity vector: ", grav_vec)
         u_ff, pd_pos = self.pin_lcc.step(
