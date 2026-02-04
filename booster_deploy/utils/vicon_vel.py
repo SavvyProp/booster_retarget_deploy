91f6522f95ad5fe0035dd9f0c890d0f36f912f05
SavvyProp <ludwigtaycheeying@gmail.com>
2026-02-04 14:40:17 -0500

    0 cmd

--- booster_deploy/utils/vicon_vel.py
+++ booster_deploy/utils/vicon_vel.py
@@ -61,7 +61,7 @@ class ViconVelocityEstimator:
 
             dt = time_since - self.last_time
             self.last_time = time_since
-            alpha = 0.50
+            alpha = 0.95
             raw_global_vel = (vicon_pos - self.vicon_pos) / dt
             self.vicon_pos = vicon_pos
             self.global_vel = self.global_vel * (1 - alpha) + raw_global_vel * alpha
