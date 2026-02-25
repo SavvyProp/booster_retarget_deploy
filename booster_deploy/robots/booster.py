42e33445ef673dd624a9d042ad2ee34f4cb183ec
SavvyProp <ludwigtaycheeying@gmail.com>
2026-02-25 15:41:27 -0500

    higher joint gain

--- booster_deploy/robots/booster.py
+++ booster_deploy/robots/booster.py
@@ -188,7 +188,7 @@ T1_23DOF_CFG = RobotCfg(
     ),
 )
 
-GAIN_FAC = 0.78
+GAIN_FAC = 0.85
 DAMP_FAC = 1.0
 
 T1_23DOF_LCC_CFG = RobotCfg(
@@ -256,15 +256,15 @@ T1_23DOF_LCC_CFG = RobotCfg(
         19.7392 * GAIN_FAC,  # Right_Shoulder_Roll
         19.7392 * GAIN_FAC,  # Right_Elbow_Pitch
         19.7392 * GAIN_FAC,  # Right_Elbow_Yaw
-        39.4784 * GAIN_FAC * 1.05,  # Waist
+        39.4784 * GAIN_FAC,  # Waist
         98.6960 * GAIN_FAC,  # Left_Hip_Pitch
-        39.4784 * GAIN_FAC * 1.1,  # Left_Hip_Roll
+        39.4784 * GAIN_FAC,  # Left_Hip_Roll
         39.4784 * GAIN_FAC,  # Left_Hip_Yaw
         98.6960 * GAIN_FAC,  # Left_Knee_Pitch
         19.7392 * GAIN_FAC,  # Left_Ankle_Pitch
         19.7392 * GAIN_FAC,  # Left_Ankle_Roll
         98.6960 * GAIN_FAC,  # Right_Hip_Pitch
-        39.4784 * GAIN_FAC * 1.1,  # Right_Hip_Roll
+        39.4784 * GAIN_FAC,  # Right_Hip_Roll
         39.4784 * GAIN_FAC,  # Right_Hip_Yaw
         98.6960 * GAIN_FAC,  # Right_Knee_Pitch
         19.7392 * GAIN_FAC,  # Right_Ankle_Pitch
