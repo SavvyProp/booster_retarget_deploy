4bef2070e5e22a0457a2634df49a6368536113ed
SavvyProp <ludwigtaycheeying@gmail.com>
2026-02-27 19:50:40 -0500

    300 hz

--- tasks/lcc_retarget/lcc_retarget.py
+++ tasks/lcc_retarget/lcc_retarget.py
@@ -25,7 +25,7 @@ def quat_to_rotmat_wxyz(q):
 
 CTRL_NUM = 23
 
-FREQ = 350.0
+FREQ = 300.0
 
 class LCCRetargetPolicy(Policy):
     def __init__(self, cfg, controller: BaseController):
@@ -197,7 +197,7 @@ class LCCRetargetPolicy(Policy):
         base_ang_vel = self.robot.data.root_ang_vel_b
         base_lin_vel = self.robot.data.root_lin_vel_b
         #self.joint_vel = self.joint_vel * (1 - self.alpha) + dof_vel * self.alpha
-        iters = int(0.02 * FREQ)
+        iters = round(0.02 * FREQ)
         if self.decimation_counter % iters == 0:
             obs = self.compute_observation(
                 dof_pos,
